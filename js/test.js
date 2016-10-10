30c07228ec7bd212a188b044dde0549f99d80af3
Namkai Fairfield <namkai.fairfield@gmail.com>
2016-10-10 08:58:48 -0700

    updated

--- js/test.js
+++ js/test.js
@@ -6,7 +6,7 @@ var savedX;
 var savedY;
 var savedLongitude;
 var savedLatitude;
-var image = '360-view.jpg'
+var image = 'test.jpg'
 var GLOBALS = [];
 
 function readQueryParams() {
@@ -24,26 +24,26 @@ function readQueryParams() {
 	}
 readQueryParams();
 
-// setting up the renderer
+
 var renderer = Detector.webgl ? new THREE.WebGLRenderer() : new THREE.CanvasRenderer();
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-// creating a new scene
+
 var scene = new THREE.Scene();
 
-// adding a camera
+
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000);
 camera.target = new THREE.Vector3(0, 0, 0);
 
-// creation of a big sphere geometry
+
 var sphere = new THREE.SphereGeometry(100, 100, 40);
 sphere.applyMatrix(new THREE.Matrix4().makeScale(-1, 1, 1));
 
 var loader = new THREE.TextureLoader();
 var texture = loader.load(image);
-// creation of the sphere material
+
 var sphereMaterial = new THREE.MeshBasicMaterial();
 sphereMaterial.map = texture;
 
@@ -51,7 +51,7 @@ sphereMaterial.map = texture;
 var sphereMesh = new THREE.Mesh(sphere, sphereMaterial);
 scene.add(sphereMesh);
 
-	// listeners
+
 	document.addEventListener("mousedown", onDocumentMouseDown, false);
 	document.addEventListener("mousemove", onDocumentMouseMove, false);
 	document.addEventListener("mouseup", onDocumentMouseUp, false);
@@ -66,21 +66,21 @@ scene.add(sphereMesh);
 			longitude += 0.02;
 		}
 
-				// limiting latitude from -85 to 85 (cannot point to the sky or under your feet)
+
 				latitude = Math.max(-85, Math.min(85, latitude));
 
-				// moving the camera according to current latitude (vertical movement) and longitude (horizontal movement)
+
 				camera.target.x = 400 * Math.sin(THREE.Math.degToRad(90 - latitude)) * Math.cos(THREE.Math.degToRad(longitude));
 				camera.target.y = 400 * Math.cos(THREE.Math.degToRad(90 - latitude));
 				camera.target.z = 400 * Math.sin(THREE.Math.degToRad(90 - latitude)) * Math.sin(THREE.Math.degToRad(longitude));
 				camera.lookAt(camera.target);
 
-				// calling again render function
+
 				renderer.render(scene, camera);
 				
 			}
 			
-			// when the mouse is pressed, we switch to manual control and save current coordinates
+
 			function onDocumentMouseDown(event){
 
 				event.preventDefault();
@@ -95,7 +95,7 @@ scene.add(sphereMesh);
 
 			}
 
-			// when the mouse moves, if in manual contro we adjust coordinates
+
 			function onDocumentMouseMove(event){
 
 				if(manualControl){
@@ -105,7 +105,7 @@ scene.add(sphereMesh);
 
 			}
 
-			// when the mouse is released, we turn manual control off
+
 			function onDocumentMouseUp(event){
 
 				manualControl = false;
